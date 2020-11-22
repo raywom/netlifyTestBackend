2fdcda29992ba7e3cbaf77666fe148d6b47c05ce
raywom <>
2020-11-22 17:01:39 +0100

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://infallible-colden-f7e227.netlify.app',
+  url: 'https://localhost:1337',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '92ca6efa8d1e293875059e6e98d39c6f'),
