c2042a4d7e8ed9f70dd439f433d5c03343899031
raywom <>
2020-11-22 15:59:25 +0100

    ad

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '92ca6efa8d1e293875059e6e98d39c6f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8881e203-3f28-474d-b0db-babf953f0bd3'
+};
